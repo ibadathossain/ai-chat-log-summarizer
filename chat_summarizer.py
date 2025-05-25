049a0276cef10b78859966a46c53ef868a353c75
Ibadat Hossain <ibadathossain@gmail.com>
2025-05-26 04:43:27 +0600

    Initial commit

---
+++ chat_summarizer.py
@@ -0,0 +1,23 @@
+def load_chat(file_path):
+    user_msgs = []
+    ai_msgs = []
+    
+
+    try:
+        with open(file_path , 'r' , encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.startswith("User:"):
+                    user_msgs.append(line.replace("User:" , "").strip())
+                elif line.startswith("AI:"):
+                    ai_msgs.append(line.replace("AI:" , "").strip())
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    return user_msgs , ai_msgs
+
+if __name__ == "__main__":
+    user_msgs, ai_msgs = load_chat("chat.txt")
+    print("User Messages:", user_msgs)
+    print("AI Messages:", ai_msgs)
+
+  
