0105c758509f83d44f424de357e071f2386da004
Ibadat Hossain <ibadathossain@gmail.com>
2025-05-26 08:33:56 +0600

    Keyword Analysis

--- chat_summarizer.py
+++ chat_summarizer.py
@@ -1,3 +1,20 @@
+import re
+from collections import Counter
+
+STOPWORDS = {
+    'the', 'is', 'and' , 'to' ,'you' , 'can' , 'for' , 'it',
+    'this' , 'that', 'in' , 'on' , 'i' , 'we',
+    'an' , 'are' , 'be' , 'as' ,'with', 'by', 'at' ,'from'
+}
+
+def clean_and_tokenize(messages):
+    all_words = []
+    for msg in messages:
+        words = re.findall(r'\b\w+\b', msg.lower())
+        filtered = [w for w in words if w not in STOPWORDS]
+        all_words.extend(filtered)
+    return Counter(all_words)
+
 def load_chat(file_path):
     user_msgs = []
     ai_msgs = []
@@ -22,10 +39,18 @@ def show_stats(user_msgs, ai_msgs):
     print(f"User messages: {len(user_msgs)}")
     print(f"AI messages: {len(ai_msgs)}")
 
+
 if __name__ == "__main__":
     user_msgs, ai_msgs = load_chat("chat.txt")
     print("User Messages:", user_msgs)
     print("AI Messages:", ai_msgs)
     show_stats(user_msgs, ai_msgs)
+
+    all_msgs = user_msgs + ai_msgs
+    word_count = clean_and_tokenize(all_msgs)
+    top_keywords = word_count.most_common(5)
+
+    print("\n.... Top 5 Keywords \n")
+    print([kw for kw , _ in top_keywords])
 
   
